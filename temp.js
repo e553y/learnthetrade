f28ecbc6c5dfa3fa8dd4edfc352507f355382877
e553y <esey.iyasu@gmail.com>
2018-05-09 19:27:19 -0700

    fix empty input box bug

--- temp.js
+++ temp.js
@@ -1,4 +1,8 @@
-
+/*retrieve temperatures and update corresponding elements
+takes 
+event: form imput event,
+thermometer: object to update
+*/
 function converter(event, thermometer) {
 	
 	
@@ -10,15 +14,26 @@ function converter(event, thermometer) {
 	if(event.target.id == "celcius"){
 		
 		let converted = convert(temperature,"F");
-		fahrenheit.value= temperature == ""? "" : roundTo(converted,4);
-		thermometer.updateLevel(temperature);
+		
+		if(temperature == ""){
+			fahrenheit.value = "";
+			thermometer.updateLevel(thermometer.minC);//set to minimum
+		}else{
+			fahrenheit.value = floatFix(converted,4);
+			thermometer.updateLevel(+temperature);
+		}
 		return;
 	}
 	if(event.target.id == 'fahrenheit'){
 		
 		let converted = convert(temperature,"C");
-		celcius.value= temperature == ""? "" : roundTo(converted,4);
-		thermometer.updateLevel(temperature);
+		if(temperature == ""){
+			celcius.value = "";
+			thermometer.updateLevel(thermometer.minC);//set to minimum
+		}else{
+			celcius.value = floatFix(converted,4);
+			thermometer.updateLevel(converted);
+		}
 		return;
 	}
 }
