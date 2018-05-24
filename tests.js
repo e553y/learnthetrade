4ea4e56112d5ef2da8148d01f3381aa8466f5cef
esey <esey.iyasu@gmail.com>
2018-05-24 04:30:47 -0700

    add graph unit tests

--- tests.js
+++ tests.js
@@ -134,7 +134,6 @@ THEN DO TESTS */
 	describe("graph.js", function () {
 
 
-		})
 
 		//test constructor
 		describe("GraphCanvas( canvas, range )", function () {
@@ -145,111 +144,289 @@ THEN DO TESTS */
 				//create array with minimum and maximum values set
 				let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
 
-//GRAPH.JS
-describe("graph.js", function () {
-	//create a canvas for tests
-	let canvas = document.createElement('canvas');
-	//create array with minimum and maximum values set
-	let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
-	//test constructor
-	describe("GraphCanvas( canvas, range )", function () {
-		it("should create a graph with the given parameters ", function () {
 
-			//test if the parameters are set correctly
-			assert.equal(canvasGraphObj.canvas, canvas);
-			assert.equal(canvasGraphObj.minX, -20);
-			assert.equal(canvasGraphObj.maxX, 20);
-			assert.equal(canvasGraphObj.minY, -15);
-			assert.equal(canvasGraphObj.maxY, 15);
+				//test if the parameters are set correctly
+				assert.equal(canvasGraphObj.canvas, canvas);
+				assert.equal(canvasGraphObj.minX, -20);
+				assert.equal(canvasGraphObj.maxX, 20);
+				assert.equal(canvasGraphObj.minY, -15);
+				assert.equal(canvasGraphObj.maxY, 15);
+
+			})
+		})
+
+		//test coordinate to pixel position converter
+		describe("getPixelPos( coordArr )", function () {
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
+
+
+
+			it("should return pixel coordinates of x-y value", function () {
+				//coordinates and thier expected corresponding pixel pos
+				let testCoordinates = new Map([ //can be added to in the future
+					[[-20, -15], [0, canvasGraphObj.canvasHeight]],
+					[[-20, 15], [0, 0]],
+					[[20, 15], [canvasGraphObj.canvasWidth, 0]],
+					[[20, -15], [canvasGraphObj.canvasWidth, canvasGraphObj.canvasHeight]],
+
+				])
+
+				for (let entry of testCoordinates) {
+
+					let arguments = entry[0];
+					let expected = entry[1];
+					assert.deepEqual(canvasGraphObj.getPixelPos(arguments), expected);
+				}
+
+			})
 
 		})
-	})
 
-	//test coordinate to pixel position converter
-	describe("getPixelPos( coordArr )", function () {
-		it("should return pixel coordinates of x-y value", function () {
-			//coordinates and thier expected corresponding pixel pos
-			let testCoordinates = new Map([ //can be added to in the future
-				[[-20, -15], [0, canvasGraphObj.canvasHeight]],
-				[[-20, 15], [0, 0]],
-				[[20, 15], [canvasGraphObj.canvasWidth, 0]],
-				[[20, -15], [canvasGraphObj.canvasWidth, canvasGraphObj.canvasHeight]],
 
-			])
+		//test pixel to coordinate converter
+		describe("getCoordVal( pixelCoordArr )", function () {
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
 
+			let testCoordinates = [ //can be added to in the future
+				{
+					expected: [-20, -15],
+					input: [0, canvasGraphObj.canvasHeight],
+					desc: "top left corner",
+				},
+				{
+					expected: [-20, 15],
+					input: [0, 0],
+					desc: "bottom left corner",
+				},
+				{
+					expected: [20, 15],
+					input: [canvasGraphObj.canvasWidth, 0],
+					desc: "top right corner",
+				},
+				{
+					expected: [20, -15],
+					input: [canvasGraphObj.canvasWidth, canvasGraphObj.canvasHeight],
+					desc: "bottom right corner",
+				},
+
+			];
 			for (let entry of testCoordinates) {
 
-				let arguments = entry[0];
-				let expected = entry[1];
-				assert.deepEqual(canvasGraphObj.getPixelPos(arguments), expected);
+				it(`should return correct values for coordinates of ${entry.desc}`, function () {
+					assert.deepEqual(canvasGraphObj.getCoordVal(entry.input), entry.expected);
+				})
+
 			}
 
-		})
+		});
 
-	})
-	//test pixel to coordinate converter
-	describe("getCoordVal( pixelCoordArr )", function () {
-		it("should return x - y coordinates of pixel position array", function () {
-			let testCoordinates = new Map([ //can be added to in the future
-				[[-20, -15], [0, canvasGraphObj.canvasHeight]],
-				[[-20, 15], [0, 0]],
-				[[20, 15], [canvasGraphObj.canvasWidth, 0]],
-				[[20, -15], [canvasGraphObj.canvasWidth, canvasGraphObj.canvasHeight]],
+		//test range inclusion evaluator
+		describe("isInXRange( x )", function () {
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
+			//test cases
+			let testXPositions = [
+				{
+					input: -20,
+					expected: false,
+					desc: "x axis minimum "
+				},
+				{
+					input: -19.99,
+					expected: true,
+					desc: "inside x-axis range "
+				},
+				{
+					input: -21,
+					expected: false,
+					desc: "outside x-axis range "
+				},
+				{
+					input: 20,
+					expected: false,
+					desc: "x axis maximum "
+				},
 
-			])
-			for (let entry of testCoordinates) {
-
-				let arguments = entry[1];
-				let expected = entry[0];
-
-				assert.deepEqual(canvasGraphObj.getCoordVal(arguments[0], arguments[1]), expected);
-			}
-
-		})
-	})
-	
-	describe("isInXRange( x )", function () {
-		it("should return if x is withing the range of graph", function () {
-			let testXPositions = new Map([
-				[-20, false ],//the edge should not be considered in range
-				[-19.999, true],
-				[0, true],
-				[19.999, true],
-				[20,false],
-			])
+			];
+			//dynamically generate test cases
 			for (let entry of testXPositions) {
 
-				let arguments = entry[0];
-				let expected = entry[1];
-
-				assert.strictEqual(canvasGraphObj.isInXRange(arguments), expected);
+				it(`should return ${entry.expected} if input is ${entry.desc}`, function () {
+					assert.strictEqual(canvasGraphObj.isInXRange(entry.input), entry.expected);
+				})
 			}
+
 		})
-	})
-	describe("isInYRange( y )", function () {
-		it("should return if y is within the range of graph", function () {
-			let testXPositions = new Map([
-				[-15, false ],//the edge should not be considered in range
-				[-14.999, true],
-				[0, true],
-				[14.999, true],
-				[15,false],
-			])
-			for (let entry of testXPositions) {
 
-				let arguments = entry[0];
-				let expected = entry[1];
 
-				assert.strictEqual(canvasGraphObj.isInYRange(arguments), expected);
+		describe("isInYRange( y )", function () {
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
+			//test cases
+			let testYPositions = [
+				{
+					input: -15,
+					expected: false,
+					desc: "y axis minimum "
+				},
+				{
+					input: -14.99,
+					expected: true,
+					desc: "inside y-axis range "
+				},
+				{
+					input: -16,
+					expected: false,
+					desc: "outside y-axis range "
+				},
+				{
+					input: 15,
+					expected: false,
+					desc: "y axis maximum "
+				},
+
+			];
+			//dynamically generate test cases
+			for (let entry of testYPositions) {
+
+				it(`should return ${entry.expected} if input is ${entry.desc}`, function () {
+					assert.strictEqual(canvasGraphObj.isInYRange(entry.input), entry.expected);
+				});
 			}
+
 		})
+
+
+
+		describe("clearGraph()", function () {
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
+
+			it("should clear the entire canvas", function () {
+
+				let graphSpy = sinon.spy(canvasGraphObj.ctx, "clearRect");
+
+				canvasGraphObj.clearGraph();
+				//check if ctx.clearRect is called with correct arguments
+				assert(graphSpy.calledWith(0, 0, canvasGraphObj.canvasWidth, canvasGraphObj.canvasHeight));
+				//remove spy wrapper
+				graphSpy.restore();
+			})
+		})
+
+
+		describe("updateRange( range )", function () {
+
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
+
+			let spies;
+
+			beforeEach(function () {
+				//prepare spies
+				spies = [
+					sinon.spy(canvasGraphObj, "clearGraph"),
+					sinon.spy(canvasGraphObj, "drawGrid"),
+					sinon.spy(canvasGraphObj, "drawAxes"),
+					sinon.spy(canvasGraphObj, "drawEquations")
+				];
+
+			});
+			//destroy elements and objects after each function test
+			afterEach(function () {
+
+				for (spy of spies) {
+					spy.restore()
+				}
+
+			});
+
+
+			it("should update range of graph with range array", function () {
+
+				let testRange = [-10, 10, -5, 5];
+				canvasGraphObj.updateRange(testRange);
+
+				assert.equal(canvasGraphObj.minX, -10, "minimum X  not set correctly");
+				assert.equal(canvasGraphObj.maxX, 10, "maximum X not set correctly");
+				assert.equal(canvasGraphObj.minY, -5, "minimum Y not set correctly");
+				assert.equal(canvasGraphObj.maxY, 5, "maximum Y not set correctly");
+			});
+
+			it("should redraw graph", function () {
+				//call updateRange
+				canvasGraphObj.updateRange([-2, 2, -10, 10]);
+				//check if it calls all spies
+				for (let spy of spies) {
+					assert(spy.called);
+				}
+			});
+
+
+		})
+
+		describe("drawEquation()", function () {
+
+			//create a canvas for tests
+			let canvas = document.createElement('canvas');
+			//create array with minimum and maximum values set
+			let canvasGraphObj = new GraphCanvas(canvas, [-20, 20, -15, 15]);
+
+			let spies;
+
+			beforeEach(function () {
+				//prepare spies
+				spies = new Map();
+				for (property in canvasGraphObj.ctx) {
+
+					//check if property is a function
+					if (typeof canvasGraphObj.ctx[property] == "function") {
+						//add spy to collection map
+						spies.set(property, sinon.spy(canvasGraphObj.ctx, property));
+					}
+				}
+			});
+			//destroy spies each function test
+			afterEach(function () {
+
+				for (spy of spies.values()) {
+					spy.restore()
+				}
+
+			});
+
+			//it should save ctx first
+			it("should save context before anything else", function () {
+				console.log(spies.get("save").called);
+				//call SUT
+				canvasGraphObj.drawEquations();
+				console.log(spies.get("save").called);
+				assert(spies.get("save").called);
+
+			})
+		})
+
+
+
 	})
-	
+	//THERMOMETER.JS
+	describe("thermometer Guage", function () {
+		describe("constructor( canvas, optionsObj )", function () {
 
-})
-//THERMOMETER.JS
-describe("thermometer Guage", function () {
-	describe("constructor( canvas, optionsObj )", function () {
-
+		})
 	})
 })
